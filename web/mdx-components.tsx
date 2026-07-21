e2c9703d6a74c58500565f5553ab92be85e8f1c2
Yolaine <tracy.ngot@gmail.com>
2026-07-21 13:41:04 +0100

    ✨ feat: render the docs architecture flow as a styled diagram

--- web/mdx-components.tsx
+++ web/mdx-components.tsx
@@ -1,11 +1,13 @@
 import type { MDXComponents } from "mdx/types";
 import { CodeBlock } from "@/components/docs/code-block";
+import { FlowDiagram } from "@/components/docs/flow-diagram";
 import { Callout, Endpoint } from "@/components/docs/mdx-elements";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     Callout,
     Endpoint,
+    FlowDiagram,
     pre: CodeBlock,
     ...components,
   };
